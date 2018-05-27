a6858605ae8c54c3c3baa2f8def4df22eeedb06c
Elsa Hsu <elsanfhsu@gmail.com>
2018-05-27 21:13:47 +0300

    Update README and comments

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+Simple arcade game made as an exercise for Udacity Front-End Web Developer
+Nanodegree Program. The base code and resources for the exercise are from
+here: https://github.com/udacity/frontend-nanodegree-arcade-game
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+To run simply dowload the files and open index.html file with your favorite
+browser. The game starts automatically.
+
+You can move your character with arrow keys. Your objective is to move your
+character up across the road and into the blue water on the other side of the
+road without getting hit by the beetles running along the road. Good luck!
+
--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 
-const COLUMN_WIDTH = 101;
-const ROW_HEIGHT = 83;
+const COLUMN_WIDTH = 101; // Width of column in grid (tile width)
+const ROW_HEIGHT = 83; // Height of column in grid (tile height)
 const NUM_COLUMNS = 5;
 const NUM_ROWS = 6;
+const NUM_ENEMIES = 3;
 const PLAYER_CHARACTERS = [
     'images/char-boy.png',
     'images/char-cat-girl.png',
@@ -13,9 +14,6 @@ const PLAYER_CHARACTERS = [
 
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -25,9 +23,7 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // Move along x-axis by speed multiplied by dt. Reset if gone over side.
     if (this.x < NUM_COLUMNS * COLUMN_WIDTH) {
         this.x += this.speed * dt;
     } else {
@@ -53,17 +49,14 @@ Enemy.prototype.render = function() {
 // Reset enemy to the left of the grid and assign row and speed
 Enemy.prototype.reset = function() {
     this.row = Math.floor(Math.random() * 3) + 1; // Integer between 1 and 3
-    // Randomize speed. More times the player has won, the faster the enemies.
+    // Randomize speed. Speed can be faster the more times player has won.
     this.speed = 50 + Math.random() * (50 * (player.win_count + 1));
     this.x = - COLUMN_WIDTH; // Start from beyond the canvas edge
     this.y = this.row * ROW_HEIGHT - 20;
     console.log("Beetle row: ", this.row, " speed: ", this.speed);
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
+// Player class. Stores player location, score and handles keyboard input.
 var Player = function() {
     this.win_count = 0;
     this.lose_count = 0;
@@ -72,12 +65,10 @@ var Player = function() {
 };
 
 Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // Player moves by steps in grid, no smooth time-based movement needed
 };
 
-// Draw the player on the screen, required method for game
+// Draw the player and score on the screen
 Player.prototype.render = function() {
     x = this.column * COLUMN_WIDTH;
     y = this.row * ROW_HEIGHT - 20; // Add a little offset for more 3D look
@@ -146,6 +137,12 @@ function updateScore() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var player = new Player();
+var allEnemies = [];
+
+for (let i = 0;i < NUM_ENEMIES;i++) {
+    allEnemies.push(new Enemy());
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -159,11 +156,4 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-var player = new Player();
-var allEnemies = [];
-
-for (let i = 0;i < 3;i++) {
-    allEnemies.push(new Enemy());
-}
 
